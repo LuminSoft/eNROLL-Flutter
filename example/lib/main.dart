1711af6962bde6eae1eac37a73e0af72a70c90e6
luminsoft <andrew_samir93@hotmail.com>
2024-09-03 17:52:58 +0300

    add correlation id

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,9 +25,9 @@ class _MyAppState extends State<MyApp> {
         return Scaffold(
           body: EnrollPlugin(
             mainScreenContext: context,
-            tenantId: 'TENANT_ID',
-            tenantSecret: 'TENANT_SECRET',
-            enrollMode: EnrollMode.auth,
+            tenantId: '3bab5a01-b3e2-4900-890c-d5fc6990e610',
+            tenantSecret: 'e84e5d36-ede2-42a6-abba-ae01a9b773fc',
+            enrollMode: EnrollMode.onboarding,
             enrollEnvironment: EnrollEnvironment.staging,
             localizationCode: EnrollLocalizations.en,
             onSuccess: () {
@@ -52,6 +52,7 @@ class _MyAppState extends State<MyApp> {
             skipTutorial: false,
             levelOfTrust: 'LEVEL_OF_TRUST_TOKEN',
             googleApiKey: 'GOOGLE_API_KEY',
+            correlationId: 'correlationIdTest',
           ),
         );
       }),
